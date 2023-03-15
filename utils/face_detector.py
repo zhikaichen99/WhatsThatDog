c4a6117a36a2fdd196d7fbea5fab221d66a06fb8
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-15 10:00:21 -0400

    function to detect if there is a face in image

---
+++ utils/face_detector.py
@@ -0,0 +1,11 @@
+import cv2
+
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_alt.xml')
+
+# returns "True" if face is detected in image stored at img_path
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
