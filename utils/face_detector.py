63c14092287c1f1e165fe4612a7eedfa61d49737
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-15 16:44:32 -0400

    added comments and docstring

--- utils/face_detector.py
+++ utils/face_detector.py
@@ -5,7 +5,19 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 
 # returns "True" if face is detected in image stored at img_path
 def face_detector(img_path):
+    """
+    Detects if there is a face in the image of the specified file path
+
+    Inputs:
+        img_path (str): file path of the image file
+    Outputs:
+        (bool): True if a face is detected in the image, false otherise
+    """
+    # load the image
     img = cv2.imread(img_path)
+    # Convert the image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces using the pre-trained face detector
     faces = face_cascade.detectMultiScale(gray)
+    # Return True if face is detected. False otherwise
     return len(faces) > 0
