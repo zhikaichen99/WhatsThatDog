6c2cb9167c26d1861a1f4e97388fcf576e871a9b
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-15 09:56:00 -0400

    function to detect if there is a dog in an image

---
+++ utils/dog_detector.py
@@ -0,0 +1,18 @@
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from utils.preprocess import path_to_tensor, paths_to_tensor
+import numpy as np
+
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+### returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151)) 
