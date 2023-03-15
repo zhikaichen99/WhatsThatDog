31e281f9f30996506794f2f82ddb73d91b8ab4b6
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-15 16:38:00 -0400

    debugging

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import tempfile
 
 from utils.extract_bottleneck_features import *
 from utils.face_detector import face_detector
@@ -20,7 +21,7 @@ model = load_model('saved_models/weights.best.VGG19.hdf5')
 
 def VGG19_predict_breed(image):
     # extract bottleneck features
-    bottleneck_feature = extract_VGG19(path_to_tensor(image))
+    bottleneck_feature = extract_VGG19(path_to_tensor(image).astype('float32')/255)
     # obtain predicted vector
     predicted_vector =  model.predict(bottleneck_feature)
     # return dog breed that is predcted by the model
@@ -28,7 +29,7 @@ def VGG19_predict_breed(image):
 
 def predict_dog_breed(file):
     if face_detector(file):
-        dog_breed = model(file)
+        dog_breed = VGG19_predict_breed(file)
         return "The resembling dog breed of the human is {}".format(dog_breed)
     elif dog_detector(file):
         dog_breed = VGG19_predict_breed(file)
@@ -36,15 +37,29 @@ def predict_dog_breed(file):
     else:
         return "Error: No dog or human face was detected in the image"
 
+
+
 if __name__ == "__main__":
 
     file = st.file_uploader("Upload an image")
     
     if file is not None:
 
-        img = Image.open(file)
-        img = img.save('img.jpg')
-        file_path = 'img.jpg'
+        with tempfile.NamedTemporaryFile(delete = False) as tmp_file:
+            tmp_file.write(file.read())
+            file_path = tmp_file.name
+        print("HERE")
+        print(file_path)
 
+        # img = Image.open(file)
+        # img = img.save('img.jpg')
+        # file_path = 'img.jpg'
+
+        st.image(file, use_column_width = True)
+
+        prediction_header = st.empty()
+
+        prediction_header.header("Loading Prediction")
+ 
         prediction = predict_dog_breed(file_path)
-        st.write(prediction)
+        prediction_header.header(prediction)
