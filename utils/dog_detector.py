439a209306f00d244ff0e0f934286cd4b14ecc6e
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-15 20:09:04 -0400

    added comments and docstring

--- utils/dog_detector.py
+++ utils/dog_detector.py
@@ -8,11 +8,28 @@ import numpy as np
 ResNet50_model = ResNet50(weights='imagenet')
 
 def ResNet50_predict_labels(img_path):
+    """
+    Returns the prediction vector for an image located at img_path
+
+    Inputs:
+        img_path (str): file path of the image file
+    Outputs:
+        Numpy array: The prediction vector for the image
+    """
     # returns prediction vector for image located at img_path
     img = preprocess_input(path_to_tensor(img_path))
     return np.argmax(ResNet50_model.predict(img))
 
 ### returns "True" if a dog is detected in the image stored at img_path
 def dog_detector(img_path):
+    """
+    Return True if a dog is detected in the image stored at img_path
+
+    Inputs:
+        img_path (str): file path of the image file
+    Ouputs:
+        bool: True if a dog is detected. False otherwise
+    """
     prediction = ResNet50_predict_labels(img_path)
+    # dog breeds are classified between index 151 and 268, hence the condition
     return ((prediction <= 268) & (prediction >= 151)) 
