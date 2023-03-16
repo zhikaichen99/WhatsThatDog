4811a879e5fdeb74232770c8ed3d93270fcea661
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-15 20:16:07 -0400

    added comments and docstring

--- app.py
+++ app.py
@@ -11,15 +11,26 @@ from keras.models import load_model
 
 from PIL import Image
 
+
+# get a list of dog breeds
 with open('dogbreeds.txt', 'r') as f:
     dog_names = f.readlines()
 
 dog_names = [x.strip() for x in dog_names]
 
+# load the pre-trained VGG19 model from the saved model folder
 model = load_model('saved_models/weights.best.VGG19.hdf5')
 
 
 def VGG19_predict_breed(image):
+    """
+    Returns the predicted breed of a dog in the image
+
+    Inputs:
+        image (str): file path of the image file
+    Outputs:
+        str: The predicted breed of the dog in the image
+    """
     # extract bottleneck features
     bottleneck_feature = extract_VGG19(path_to_tensor(image).astype('float32')/255)
     # obtain predicted vector
@@ -28,6 +39,14 @@ def VGG19_predict_breed(image):
     return dog_names[np.argmax(predicted_vector)]
 
 def predict_dog_breed(file):
+    """
+    Returns a string describing the predicted dog breed or an error message
+
+    Inputs:
+        file (str): file path of the image file
+    Outputs:
+        str: The predicted dog breed or an error message if no dog or human face detected
+    """
     if face_detector(file):
         dog_breed = VGG19_predict_breed(file)
         return "The resembling dog breed of the human is {}".format(dog_breed)
@@ -44,22 +63,20 @@ if __name__ == "__main__":
     file = st.file_uploader("Upload an image")
     
     if file is not None:
-
+        # save the uploaded file to a temporary file on disk
         with tempfile.NamedTemporaryFile(delete = False) as tmp_file:
             tmp_file.write(file.read())
             file_path = tmp_file.name
         print("HERE")
         print(file_path)
 
-        # img = Image.open(file)
-        # img = img.save('img.jpg')
-        # file_path = 'img.jpg'
-
+        # display the uploaded image on the streamlit app
         st.image(file, use_column_width = True)
 
+        # display a header indicating the prediction is being loaded
         prediction_header = st.empty()
-
         prediction_header.header("Loading Prediction")
  
+        # predict the dog breed from the uploaded image and display the results
         prediction = predict_dog_breed(file_path)
         prediction_header.header(prediction)
